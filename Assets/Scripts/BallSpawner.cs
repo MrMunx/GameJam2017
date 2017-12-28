26a1ee8276e11e69cdb10da899ac226d08b078ae
MrMunx <mini.duncan@gmail.com>
2017-12-28 20:19:53 +0000

    Added first worms, skyscrapers, ball spawner

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    public Box box;
+    private GameObject collidingObject;
+    // 2
+    private GameObject objectInHand;
+    private  GameObject ball;
+    private float newPosition;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        ball = GameObject.Find("itchbal");
+        collidingObject = GameObject.Find("box1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition = trackedObj.transform.position.y + 2;
+
+        if (Controller.GetHairTriggerDown())
+        {
+            if (box.boxPrepareSpawn)
+            {
+                Instantiate(ball, new Vector3(trackedObj.transform.position.x, newPosition, trackedObj.transform.position.z), ball.transform.rotation);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    public bool boxPrepareSpawn;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        boxPrepareSpawn = true;
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        boxPrepareSpawn = false;
+    }
+}
